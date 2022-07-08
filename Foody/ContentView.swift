bdb5da65cc25d5abab1c220d43820438b004e6a0
dekapd99 <dekaprimatio@gmail.com>
2022-07-09 00:51:26 +0700

    Initial Commit

---
+++ Foody/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foody
+//
+//  Created by Deka Primatio on 09/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foody/FoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodyApp.swift
+//  Foody
+//
+//  Created by Deka Primatio on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
