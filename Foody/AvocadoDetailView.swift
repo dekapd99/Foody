2c3cca7f5da2d434ee1c781c9c29a6fd3b0f21ac
dekapd99 <dekaprimatio@gmail.com>
2022-08-02 10:04:55 +0700

    2 halaman detail baru + image set

---
+++ Foody/AvocadoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadoDetailView.swift
+//  Foody
+//
+//  Created by Deka Primatio on 02/08/22.
+//
+
+import SwiftUI
+
+struct AvocadoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvocadoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadoDetailView()
+    }
+}
---
+++ Foody/OrangeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  OrangeDetailView.swift
+//  Foody
+//
+//  Created by Deka Primatio on 02/08/22.
+//
+
+import SwiftUI
+
+struct OrangeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrangeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrangeDetailView()
+    }
+}
